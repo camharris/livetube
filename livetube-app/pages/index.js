0da8794bd46bd4d07333ef861754ad458976c7de
Cameron Harris <charris@planview.com>
2021-05-30 00:06:00 -0500

    working home page in ui

--- livetube-app/pages/index.js
+++ livetube-app/pages/index.js
@@ -1,63 +1,65 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Home() {
+export async function getServerSideProps() {
+  const library = [];   
+  const slateAPI = 'https://slate.host/api/v2/get';
+  const response = await fetch(slateAPI, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      //Authorization: 'Basic ' + process.env.NEXT_PUBLIC_SLATE_API_KEY,
+      Authorization: 'Basic SLAe55e06d4-c873-43f1-b76b-d8937054e218TE'
+    }
+  })
+  const data = await response.json()
+
+  if (!data) {
+    console.log("NO RESPONSE FROM SLATE API!!!")
+  }
+  
+  if (data.error) {
+    console.log(data);
+  } else {
+    const library = data;
+  }
+  return {
+      props: {
+          data,
+      }
+  } 
+}
+
+export default function Home({ data }) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>liveTube App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <img  src="/livepeer_logo.png"/>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          What would you like to watch? 
         </p>
-
+        
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          {data.user.library.map(item => (
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <a key={item.id} href="/video" className="card">
+              <img src="/default_video_thumb.png" sizes="(max-width: 100px) 100px" width="300"/>
+              <h4>{item.data.name}</h4>
+              <h5>{item.data.body}</h5>
+            </a>
+          ))}
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -207,3 +209,7 @@ export default function Home() {
     </div>
   )
 }
+
+
+
+
---
+++ livetube-app/pages/video.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Video() {
+    return (
+    <div className="container">
+      <Head>
+        <title>LiveTube</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Video Playback</h1>
+
+      </main>
+
+    </div>
+    )
+}
